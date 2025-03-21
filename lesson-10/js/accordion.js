94f4bd8c52e08e3879bd81dcea5c2df04af0d700
janetpiao <janetpiao66@gmail.com>
2025-03-21 11:00:03 -0400

    completed L10

--- lesson-10/js/accordion.js
+++ lesson-10/js/accordion.js
@@ -1,14 +1,18 @@
 // STEP 6: Nab all the SUMMARY elements
+const details = document.querySelectorAll("details");
 
-
-// STEP 7: Loop through the element array to add an event listener, and reference a function called closeOpenDetails
-
-
-// STEP 8a: Create the closeOpenDetails() function
-
-	// STEP 8b: Loop through the summaries array again
-
-		// STEp 8c: Check to make sure the DETAILS element is not the parent of the SUMMARY that was clicked 
-
-
+// STEP 7: Loop through details array and add event listener for toggle
+details.forEach((detail) => {
+  detail.addEventListener("toggle", (event) => {
+    // console.log("Toggle event added!");
+    if (event.target.open) {
+      details.forEach((detail) => {
+        if (detail !== event.target) {
+          // This is not the details element that was clicked open
+          detail.removeAttribute("open");
+        }
+      });
+    }
+  });
+});
 // STEP 9: Check in the browser to make sure there are no errors - use the console, then proceed to the CSS for the final step
