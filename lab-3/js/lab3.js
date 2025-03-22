d9d700460575e94b23e6de13e1c37ee5ef240974
janetpiao <janetpiao66@gmail.com>
2025-03-21 23:22:20 -0400

    Tried some ways, buy still can't hide and show content. At least improved the CSS for tab menu and content.

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,1 +1,27 @@
+// const tabpanels = document.querySelectorAll("tabpanel");
 
+// tabpanels.forEach((tabpanel) => {
+//   tabpanel.addEventListener("toggle", (event) => {
+//     // console.log("Toggle event added!");
+//     if (event.target.open) {
+//       tabpanels.forEach((tabpanel) => {
+//         if (tabpanel !== event.target) {
+//           // This is not the details element that was clicked open
+//           tabpanel.removeAttribute("open");
+//         }
+//       });
+//     }
+//   });
+// });
+
+// How to select role element in JavaScript? Reference: https://stackoverflow.com/questions/64312576/how-to-select-an-element-with-attribute-role
+
+// How to use toggle to hide and show? Reference:https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+function showContent() {
+  const content = document.querySelector(`[role="tabpanel"]`);
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
